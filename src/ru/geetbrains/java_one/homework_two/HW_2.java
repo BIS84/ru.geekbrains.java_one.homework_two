a19cb4fc10ca488635534be3bb7a096fa768e9a2
BIS84 <bbiiss84@mail.ru>
2020-11-05 20:42:35 +0300

    Add Task 2

--- src/ru/geetbrains/java_one/homework_two/HW_2.java
+++ src/ru/geetbrains/java_one/homework_two/HW_2.java
@@ -22,6 +22,15 @@ public class HW_2 {
         return arr;
     }
 
+    // Task 2
+    private  static int[] arithmeticProgression(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 1 + 3 * i;
+        }
+        return arr;
+    }
+
     public   static void main(String[] args) {
 
         // 1
@@ -30,6 +39,13 @@ public class HW_2 {
         int[] arr1 = {1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1};
         int[] arr1Invert = replaceOneWithZero(arr1);
         printArrayOneDimensional(arr1Invert);
+
+        // 2
+        String multipleLines2 = System.lineSeparator() + "Task 2";
+        System.out.println(multipleLines2);
+        int[] arr2 = new int[8];
+        int[] arr2Result = arithmeticProgression(arr2);
+        printArrayOneDimensional(arr2Result);
     }
 
 }
